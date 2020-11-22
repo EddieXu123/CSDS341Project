d5149b8617117acc3ec1af9ecf4abdcf9cd41ef0
Prithik Karthikeyan <prithik2000@gmail.com>
2020-11-21 16:39:58 -0800

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+
+
+function App() {
+  return (
+    <div>
+      <h1> lol </h1>
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
