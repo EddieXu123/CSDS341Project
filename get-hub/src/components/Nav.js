f38c4901fbb7d1f8f18e67e6168080aa0e49953b
Prithik Karthikeyan <prithik2000@gmail.com>
2020-11-21 16:36:39 -0800

    components

---
+++ get-hub/src/components/Nav.js
@@ -0,0 +1,16 @@
+import React , {Component} from 'react';
+import {Navbar, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
+
+export class Nav extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Navbar</h1>
+            </div>
+        );
+    }
+}
+
+export default Nav;
+
+
