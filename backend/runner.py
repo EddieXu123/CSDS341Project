9f47f741b3eb8ff845715bab03dc643e5e12e2bc
Prithik Karthikeyan <prithik2000@gmail.com>
2020-11-29 12:02:21 -0800

    runner.py has been updated for application usage

--- backend/runner.py
+++ backend/runner.py
@@ -1,36 +1,65 @@
-from random import *
-from Student import student
+import mysql.connector
+#edit this according to your local configuration
+#python -m pip install mysql-connector-python 
+#^ make sure this package is installed using terminal so that you can run everything else
 
-database = []
+#create connection to database
+#modify this according to your setup
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  password="mysql",
+  database = "gethub"
+)
 
-#def __init__(self, name, major, gpa):
 
+mycursor = mydb.cursor()
+
+#sample SQL query format
+#sql = "INSERT INTO Login_Info (Email_address, Password) VALUES (%s, %s)"
+#val = ("pthik2000@gmail.com", "1234")
+#mycursor.execute(sql, val)
+
+#list of methods for register
 def get_first_name():
     return input('Please enter your first name')
 
 def get_last_name():
     return input('Please enter your last name')
 
-def get_user_ID(name):
-    return str(name) + "_" + str(randint(0, 1000))
-
 def get_gpa():
     return input('Please Enter your GPA on a 4.0 scale: ')
-
 
 def create_student():
     first_name = get_first_name()
     last_name = get_last_name()
-    user_id = get_user_ID()
-    gpa = get_gpa()
-    student = Student(first_name, last_name, user_id, )
 
 
 
-student = Student(, "XU", 35, "Hello", 3, 201, "English", 36, 36, 2)
-print(student)
 
-print(create_student())
-print
+
+
+
+#application starts here
+key = 1
+print("Welcome to Gethub, the real antisocial social club")
+while( key >= 1):
+    print("press 1 to login")
+    print("press 2 to register")
+    print("press 0 to quit")
+    val = input()
+    if(int(val) > 2):
+        print("invalid value, try again")
+    else if(int(val) == 0):
+        key = int(val)
+    else:
+        key = int(val)
+        if(key == 1):
+            #login()
+        else:
+            #register()
+
+
+    
 
 
