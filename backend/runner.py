2eaf0c765edeb31d0ab736eade07044c1cfd502c
Prithik Karthikeyan <prithik2000@gmail.com>
2020-12-05 18:33:59 -0800

    orhanbae

--- backend/runner.py
+++ backend/runner.py
@@ -8,8 +8,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="mysql",
-  database = "gethub"
+  password="orhanisbae",
+  database = "test"
 )
 
 
